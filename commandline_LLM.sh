5e878c5b317ccaf522bc1ce1ef91059ddbcec464
marinazanoni <146860952+marinazanoni@users.noreply.github.com>
2023-11-04 20:09:23 +0100

    Add files via upload

---
+++ commandline_LLM.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+
+#we submitted the original script to ChatGPT and among various implementation #recommendations we selected the following:
+ 
+#a check to ensure that the jq command is available in the system
+command -v jq &> /dev/null || { echo "the command jq is not available."; exit 1; }
+
+
+#a check to verify that the series.json file exists
+[ -f "series.json" ] || { echo "the file series.json does not exist."; exit 1; }
+
+
+#the original core of the script
+cat series.json | jq -r '. | [.id, .title, (.works | map(.books_count | tonumber) | add)] | @tsv' | sort -t $'\t' -k3,3nr | head -n 5 | awk -F '\t' 'BEGIN { printf "%-8s %-40s %-20s\n", "id", "title", "total_books_count"} { printf "%-8s %-40s %-20s\n", $1, $2, $3 }'
---
+++ commandline_original.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+
+#this bash script reads the series.json file and uses the jq command to extract information #from the file such as id,title and the total book count in the works section.
+#the extracted data is then sorted based on the total book count in descending order.
+#only the first 5 lines are selected using the head -n5 command.
+#Finally, the data is formatted using the awk command to present the results in a table with aligned columns.
+
+
+cat series.json | jq -r '. | [.id, .title, (.works | map(.books_count | tonumber) | add)] | @tsv' |
+sort -t $'\t' -k3,3nr | head -n 5 | awk -F '\t' 'BEGIN { printf "%-8s %-40s %-20s\n", "id", "title",
+ "total_books_count"} { printf "%-8s %-40s %-20s\n", $1, $2, $3 }'
